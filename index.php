c783230817a56a17b3f910597f0e91c1ed0ca085
Jason Sherwin <jason@inrgb.com>
2015-03-25 16:27:38 -0700

    added favicon

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
 
         <link rel="stylesheet" href="./assets/styles/style.min.css">
 
+        <link rel="icon" href="favicon.ico" type="image/x-icon">
+
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="./assets/js/jquery-1.11.2.min.js"><\/script>')</script>
     </head>
